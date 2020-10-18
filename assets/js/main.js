fbd8b14bccc2016d2736070a7da3bba78ed635b9
zerckel <antoinecarbonnel@yahoo.fr>
2020-10-18 23:53:10 +0200

    fin page contact.html + script pour zoom image et responsive index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,4 @@
-document.querySelector('#know-howPage #know-how .right-sect') !== null ? knowHowAnim() : false
-
-
+// Ajout d'une class au scroll de la nav
 window.addEventListener('scroll', function () {
   if (window.scrollY === 0) {
     document.querySelector('#nav').classList.remove('onScroll')
@@ -8,16 +6,24 @@ window.addEventListener('scroll', function () {
     document.querySelector('#nav').classList.add('onScroll')
   }
 })
+
+// cale a droite le scroll de la div de la homepage
 setTimeout(function () {
   document.querySelector('#panoramique .scroll').scrollLeft = 448
 }, 500)
 
+// Select tout elem d'une class et leur attribut un eventListener avec une fonction
+function setEventOnAll(listElem) {
+  listElem.forEach((element) => element.addEventListener("click", fullScreenImg))
+}
 
-function knowHowsAnim() {
-  document.querySelector('#know-howPage #know-how .right-sect').addEventListener('mouseenter', function () {
-    document.querySelector('#know-howPage #know-how .right-sect .text').style.right = '40%'
-  })
-  document.querySelector('#know-howPage #know-how .right-sect').addEventListener('mouseleave', function () {
-    document.querySelector('#know-howPage #know-how .right-sect .text').style.right = 'unset'
+
+function fullScreenImg() {
+  document.querySelector('#fog .imgMax').src = this.src
+  document.querySelector('#fog').style.display = "flex"
+  document.querySelector('#fog').addEventListener('click', function () {
+    document.querySelector('#fog').style.display = "none"
   })
 }
+
+setEventOnAll(document.querySelectorAll('.imgZoom'))
